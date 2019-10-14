b27e9ee71593f4c8b651b081d58d13e9e0348f1e
Saiful <msisaifulsaif@gmail.com>
2019-10-15 00:28:16 +0600

    binary searching

---
+++ DALGO/Searching/Binary.php
@@ -0,0 +1,43 @@
+<?php
+namespace Searching;
+
+class Binary{
+  private $size;
+  private $element;
+
+  public function __construct($element){
+    $this->element = $element;
+  }
+
+  public function isExist(iterable  $collection): int{
+    $this->size = sizeof($collection);
+
+    $first = 0;
+    $last = $this->size - 1;
+    $middle = ($first+$last)/2;
+
+    while($first <= $last){
+      if($collection[$middle] < $this->element){
+        $first = $middle + 1;
+      }elseif($collection[$middle] == $this->element){
+        return 1;
+      }else{
+        $last = $middle -1;
+      }
+
+      $middle = ($first+$last)/2;
+    }
+    return -1;
+  }
+
+}
+
+//Find exist value with Binary search
+
+$arr = [1,2,3,4,5,6,7,8,9];
+$findElem = new Binary(8);
+echo $findElem->isExist($arr);
+
+$arr = ['a', 'b', 'c', 'd'];
+$findElem = new Binary('c');
+echo $findElem->isExist($arr);
